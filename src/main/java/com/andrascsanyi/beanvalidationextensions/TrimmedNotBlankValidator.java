de29eff4a049e43d464de6e81eb2d7e68e840247
Andras Csanyi <andras.csanyi@pm.me>
2024-07-05 18:35:33 +0200

    trimmed not blank error messages and tests

--- src/main/java/com/andrascsanyi/beanvalidationextensions/TrimmedNotBlankValidator.java
+++ src/main/java/com/andrascsanyi/beanvalidationextensions/TrimmedNotBlankValidator.java
@@ -5,48 +5,24 @@ import jakarta.validation.ConstraintValidatorContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.andrascsanyi.beanvalidationextensions.ValidationHelpers.getPackageAndAnnotationName;
+public class TrimmedNotBlankValidator implements ConstraintValidator<TrimmedNotBlank, String> {
 
-public class TrimmedNotBlankValidator
-    implements ConstraintValidator<TrimmedNotBlank, String> {
-    
     private final static Logger log = LoggerFactory.getLogger(TrimmedNotBlankValidator.class);
-    
+    private String message;
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("{")
-                .append(getPackageAndAnnotationName(TrimmedNotBlank.class))
-                .append(".")
-                .append("message=")
-                .append("The provided string is null. Validation cannot be executed.")
-                .append("}");
+
+        if (value == null || value.trim().isBlank()) {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(builder.toString()).addConstraintViolation();
-            return false;
-            
-        }
-        
-        if (value.trim().isBlank()) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("{")
-                .append(getPackageAndAnnotationName(TrimmedNotBlank.class))
-                .append(".")
-                .append("message=")
-                .append("When the provided string (")
-                .append(value)
-                .append(") is trimmed, it cannot be blank.")
-                .append("}");
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(builder.toString()).addConstraintViolation();
+            context.buildConstraintViolationWithTemplate(message).addConstraintViolation();
             return false;
         }
         return true;
     }
-    
+
     @Override
     public void initialize(TrimmedNotBlank constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
+        message = constraintAnnotation.message();
     }
 }
